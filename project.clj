c3e75d307416c625b2092e32266d45724ba52991
Timothy Dean <timothy.dean@workiva.com>
2018-09-26 17:09:29 -0500

    Initial repo setup: license and lein-shell

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject tesserae "1.0.0"
   :description "Tesserae"
   :url "https://github.com/Workiva/tesserae"
+  :license {:name "Eclipse Public License 1.0"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.1"]]
-  :plugins [[lein-codox "0.10.4"]]
+  :plugins [[lein-codox "0.10.4"]
+            [lein-shell "0.5.0"]]
   
   :source-paths ["src"]
   :test-paths ["test"]
